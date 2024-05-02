29b73917c8367e0c097d4b68b6ef4c30a691a4d5
Evansmbongo <120010640+Evansmbongo@users.noreply.github.com>
2024-05-02 15:56:04 +0000

    Code

--- src/test/java/Automation.java
+++ src/test/java/Automation.java
@@ -1,5 +1,8 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -8,7 +11,38 @@ public class Automation {
     @Test
     public void test(){
         this.driver = new ChromeDriver();
-        driver.get("https://www.google.com/");
+        driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
+        driver.findElement(By.xpath("//button[normalize-space()='Bank Manager Login']")).click();
+        driver.findElement(By.xpath("//button[normalize-space()='Add Customer']")).click();
+
+        driver.findElement(By.xpath("//input[@placeholder='First Name']")).sendKeys("Evans");
+        driver.findElement(By.xpath("//input[@placeholder='Last Name']")).sendKeys("Mbongo");
+        driver.findElement(By.xpath("//input[@placeholder='Post Code']")).sendKeys("1234");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        driver.switchTo().alert().accept();
+
+        driver.findElement(By.xpath("//button[normalize-space()='Open Account']")).click();
+        WebElement customer = driver.findElement(By.xpath("//select[@id='userSelect']"));
+        Select select = new Select(customer);
+        select.selectByVisibleText("Evans Mbongo");
+
+        WebElement currency = driver.findElement(By.xpath("//select[@id='currency']"));
+        Select select1 = new Select(currency);
+        select1.selectByVisibleText("Dollar");
+
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        driver.switchTo().alert().accept();
+
+        driver.findElement(By.xpath("//button[@class='btn btn-lg tab btn-primary']")).click();
+
+
+
+
+
+
+
+
+
 
 
     }
