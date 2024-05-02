bf4f816df5ddf1e5be5d663cbbdd09f439128bed
Joel Bamfo <joel.bamfo@amalitech.com>
2024-05-02 15:54:23 +0000

    Joey

--- cypress/e2e/chapterQA.spec.cy.js
+++ cypress/e2e/chapterQA.spec.cy.js
@@ -1,5 +1,24 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  before('passes', () => {
+    cy.visit('https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login')
   })
+
+  it('Managers Portal', () => {
+    cy.get('[ng-click="manager()"]').should('exist').click();
+    cy.get('[ng-click="addCust()"]').should('exist').click();
+    cy.get('[ng-model="fName"]').should().type("Franklin");
+    cy.get('[ng-model="lName"]').should().type("Saint");
+    cy.get('[ng-model="postCd"]').should().type("1001021");
+    cy.get('[type=submit').should('have.attr', 'type', "submit").click();
+    cy.get('[ng-click="addCust()"]').should('exist').click();
+    const form = cy.get('.marTop.ng-scope');
+    form.within(()=>{
+      form.should('contain','Franklin');
+      form.should('contain','Saint');
+      form.should('contain','1001021');
+    })
+    
+
+    
+  });
 })
