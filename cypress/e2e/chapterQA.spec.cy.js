851404a1dcfae20b32f229a3c7eef80363c9faa1
Joel Bamfo <joel.bamfo@amalitech.com>
2024-05-02 16:14:37 +0000

    Joey 3

--- cypress/e2e/chapterQA.spec.cy.js
+++ cypress/e2e/chapterQA.spec.cy.js
@@ -11,11 +11,25 @@ describe('template spec', () => {
     cy.get('[ng-model="postCd"]').should().type("1001021");
     cy.get('[type=submit').should('have.attr', 'type', "submit").click();
     cy.get('[ng-click="addCust()"]').should('exist').click();
-    const form = cy.get('.marTop.ng-scope');
-    form.within(()=>{
-      form.should('contain','Franklin');
-      form.should('contain','Saint');
-      form.should('contain','1001021');
+    cy.get('table').then(($table) => {
+      // Extract the table data
+      const rows = $table.find('tbody tr');
+      
+      // Perform operations/assertions on table data
+      // For example, you can iterate over rows and extract cell data
+      rows.each((index, row) => {
+        const cells = Cypress.$(row).find('td');
+        
+        // Log cell data
+        cells.each((cellIndex, cell) => {
+          cy.log('Cell ' + cellIndex + ': ' + Cypress.$(cell).text());
+        });
+        
+        // Assert or perform operations based on cell data
+        // Example assertion
+        // cy.wrap(cells).eq(0).should('contain', 'ExpectedData');
+      });
+    });
     })
     cy.get('[ng-click="home()"]').should('be.visible').click();
     cy.get('[ng-click="customer()"]').should('exist').click();
